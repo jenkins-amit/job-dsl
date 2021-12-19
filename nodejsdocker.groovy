fe66a5fbfff5e3aa99dcb5f43f27ad35b30caf84
Amit Kumar <amitverma1150@gmail.com>
2021-12-19 17:30:06 +0530

    added node docker groovy job

---
+++ nodejsdocker.groovy
@@ -0,0 +1,23 @@
+job('nodejs docker app') {
+    scm {
+        git('https://github.com/jenkins-amit/node-demo.git') { node ->
+            node / gitConfigName('JOB DSL')
+            node / gitConfigEmail('jenkins-dsl@jenkins.com')
+        }
+    }
+
+    triggers {
+        scm('H/5 * * * *')
+    }
+
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('amit0511/nodejs-app')
+            tag('${BUILD_TIMESTAMP}-${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
